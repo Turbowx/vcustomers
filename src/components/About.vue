731a63c8199164c7bc4b860ad1c487f1e4980d37
Turbowx <1534274493@qq.com>
2023-02-15 13:06:27 +0800

    提交信息

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about container">
+    <h1 class="page-header">数据存取：JSON-SERVER本地数据接口</h1>
+    <p>伪造模拟数据先行开发，启动一个本地数据接口!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
